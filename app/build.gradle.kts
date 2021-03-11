3cc8462305a7eba39fa3cf2862b37e1a848e03d1
Rafał Borcz <rafal.borcz@goodylabs.com>
2021-03-11 02:43:39 +0100

    Update dependecies and use java 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,13 +28,14 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        isCoreLibraryDesugaringEnabled = true
+
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
-        useIR = true
+        jvmTarget = "11"
     }
 
     buildFeatures {
@@ -43,29 +44,40 @@ android {
 
     composeOptions {
         kotlinCompilerExtensionVersion = composeVersion
-        kotlinCompilerVersion = "1.4.30"
     }
 }
 
 val composeVersion = "1.0.0-beta01"
 val ktorVersion = "1.5.2"
 val koinVersion = "3.0.1-beta-1"
+val kotlinVersion: String by rootProject.extra
 
 dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.3")
+
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("com.google.android.material:material:1.2.1")
+    implementation("androidx.activity:activity-compose:1.3.0-alpha04")
+
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.material:material:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling:$composeVersion")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.0-alpha06")
-    implementation("androidx.activity:activity-compose:1.3.0-alpha02")
+
+    implementation("com.google.android.material:material:1.3.0")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.0")
+
     implementation("io.insert-koin:koin-core:$koinVersion")
     implementation("io.insert-koin:koin-android:$koinVersion")
 
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-serialization-jvm:$ktorVersion")
-    testImplementation("junit:junit:4.+")
+
+    testImplementation("junit:junit:4.13.2")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
 }
