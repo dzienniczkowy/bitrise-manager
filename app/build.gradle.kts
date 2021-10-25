f7396a2e632be9851e2c9311b4916eb01e795b8e
Rafał Borcz <rafal.borcz@goodylabs.com>
2021-10-25 13:43:57 +0200

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,14 +6,18 @@ plugins {
     id("dagger.hilt.android.plugin")
 }
 
+val composeVersion = "1.0.4"
+val ktorVersion = "1.6.2"
+val kotlinVersion: String by rootProject.extra
+val hiltVersion: String by rootProject.extra
+
 android {
-    compileSdk = 30
-    buildToolsVersion = "30.0.3"
+    compileSdk = 31
 
     defaultConfig {
         applicationId = "io.github.wulkanowy.manager"
         minSdk = 21
-        targetSdk = 30
+        targetSdk = 31
         versionCode = 1
         versionName = "0.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -32,33 +36,22 @@ android {
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
 
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 
     kapt {
         correctErrorTypes = true
     }
 
-    hilt {
-        enableExperimentalClasspathAggregation = true
-    }
-
     buildFeatures {
         compose = true
     }
-}
 
-val composeVersion = "1.0.1"
-val ktorVersion = "1.6.2"
-val kotlinVersion: String by rootProject.extra
-val hiltVersion: String by rootProject.extra
-
-android {
     composeOptions {
         kotlinCompilerExtensionVersion = composeVersion
     }
@@ -67,13 +60,12 @@ android {
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
 
     implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.activity:activity-compose:1.3.1")
-    implementation("androidx.navigation:navigation-compose:2.4.0-alpha06")
+    implementation("androidx.navigation:navigation-compose:2.4.0-alpha10")
 
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling:$composeVersion")
@@ -84,7 +76,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
 
     implementation("com.google.dagger:hilt-android:$hiltVersion")
-    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0-alpha03")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0-alpha03")
     kapt("com.google.dagger:hilt-compiler:$hiltVersion")
 
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,13 @@
 buildscript {
-    val kotlinVersion by extra("1.5.21")
+    val kotlinVersion by extra("1.5.31")
     val hiltVersion by extra("2.38.1")
-    val composeVersion by extra("1.0.1")
 
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0")
+        classpath("com.android.tools.build:gradle:7.0.3")
         classpath(kotlin("gradle-plugin", version = kotlinVersion))
         classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
         classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
