82c4c97c1e0217aa06afcaab8d38f9a3c73b35e0
Mikołaj Pich <m.pich@outlook.com>
2021-11-22 22:36:42 +0100

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling:$composeVersion")
     implementation("androidx.compose.material:material:$composeVersion")
     implementation("androidx.compose.material:material-icons-extended:1.0.5")
-    implementation("com.google.accompanist:accompanist-swiperefresh:0.21.0-beta")
+    implementation("com.google.accompanist:accompanist-swiperefresh:0.21.3-beta")
 
 
     implementation("io.coil-kt:coil-compose:1.4.0")
@@ -90,7 +90,7 @@ dependencies {
     implementation("io.ktor:ktor-client-logging:$ktorVersion")
 
     implementation("com.jakewharton.timber:timber:5.0.1")
-    implementation("ch.qos.logback:logback-classic:1.2.6")
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.20.0")
+    implementation("ch.qos.logback:logback-classic:1.2.7")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.20.2")
 
 }
